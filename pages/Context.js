38337c7504a45420a3cfd531f68e2b1170a5d4d0
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-11 11:16:56 +0300

    fetched data from the api

--- pages/Context.js
+++ pages/Context.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 const Context = React.createContext()
+const API_URL = "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=madagascar"
 export default function ContextProvider({children}) {
+    const [weather, setWeather] = useState([])
+    async function fetchWeather() {
+        const response = await fetch(API_URL)
+        const data = await response.json()
+        console.log(data);
+        setWeather(data)
+    }
+    useEffect(() => {
+        fetchWeather()
+    }, [])
     return (
-        <Context.Provider value={{}}>
+        <Context.Provider value={{weather}}>
             {children}
         </Context.Provider>
     )
