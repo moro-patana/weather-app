3712bb3aab658c877028d20ad131f5539d79a8c6
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-11 15:12:24 +0300

    imported searchCity function

--- pages/App.js
+++ pages/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import SearchCity from "../components/SearchCity"
 
 export default function App() {
     return (
         <div>
             <h1>Onja Weather App</h1>
+            <SearchCity/>
         </div>
     )
 }
