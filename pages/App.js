3384cc203bbcf10a643d6bab0b18d21cbdf34082
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-13 15:41:12 +0300

    use switch

--- pages/App.js
+++ pages/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import SearchCity from "../components/SearchCity"
+import WeatherCard from "../components/WeatherCard"
+import { Switch, Route} from "react-router-dom" 
+import TodayHightlights from '../components/TodayHightlights'
 
 export default function App() {
     return (
         <div>
             <h1>Onja Weather App</h1>
             <SearchCity/>
+            <div className="woeid">
+                <WeatherCard/>
+                <Switch>
+                    <Route path="/details/:weatherId">
+                        <TodayHightlights/>
+                    </Route>
+                </Switch>
+            </div>
         </div>
     )
 }
