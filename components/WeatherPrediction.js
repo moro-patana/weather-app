09680c6c6457835eefc200f4690fe9fc60f22a21
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-20 07:41:48 +0300

    set the setIsParams as a parameter of the weatherPrediction

--- components/WeatherPrediction.js
+++ components/WeatherPrediction.js
@@ -2,7 +2,7 @@
 import React, { useContext, useState } from 'react'
 import {Link} from "react-router-dom"
 import {Context} from "../pages/Context"
-export default function WeatherPrediction() {
+export default function WeatherPrediction({setIsParams}) {
     const {weather, isLoading, fahrenheit,setFahrenheit} = useContext(Context)
     console.log(weather);
     return (
@@ -14,7 +14,7 @@ export default function WeatherPrediction() {
         {isLoading && <p>Loading...</p>}
         <div className="weather-list">
          {weather.consolidated_weather?.slice(1).map(item => (
-        <Link to={`/details/${item.id}`} key={item.id}>
+        <Link to={`/details/${item.id}`} key={item.id} onClick={() => setIsParams(true)}>
             <div className="weather-list-item" key={item.id}>
                 <h4>{new Date(item.applicable_date).toDateString()}</h4>
                 <img src={`https://www.metaweather.com//static/img/weather/${item.weather_state_abbr}.svg`}/>                
