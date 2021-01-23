79ba1bcee6935ae77821bb7ec0080845942e4495
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-23 09:49:24 +0300

    clean the code

--- components/TemperatureSideBar.js
+++ components/TemperatureSideBar.js
@@ -8,14 +8,18 @@ export default function TemperatueSideBar() {
     
     return (
         <div className="sidebar">
-            <button className="search-button" onClick={handleSearchCity}>Search for places</button>
-            {isLoading && <p>Loading...</p>}
+            <button className="open-modal" onClick={handleSearchCity}>Search for places</button>
+            {isLoading && <p className="loading">Loading...</p>}
             {weather.consolidated_weather?.slice(0, 1).map(item => (
-            <div key={item.id}>
+            <div className="weather-today" key={item.id}>
                 <img className="weather-icon" src={`https://www.metaweather.com//static/img/weather/${item.weather_state_abbr}.svg`}/>
                 <p className="temperature">{Math.floor(Math.round(fahrenheit ? (item.max_temp * 9 / 5) + 32: item.max_temp))}&deg;{`${fahrenheit ? 'F' : "C"}`}</p>
-                <p>{item.weather_state_name}</p>
-                <p>Today {new Date(item.applicable_date).toDateString()}</p>
+                <p className="weather-name">{item.weather_state_name}</p>
+                <div className="date">
+                    <span>Today</span> 
+                    <span>.</span>
+                    <span>{new Date(item.applicable_date).toDateString()}</span>
+                </div>
                 <div className="location">
                     <img className="icon" src={LocationMap} alt="icon for the location map"/>
                     <p>{query}</p>
