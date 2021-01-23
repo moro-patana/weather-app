0a173964e57378c8d371497842d28a9298b8b896
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-23 09:47:24 +0300

    gave classname

--- components/WeatherPrediction.js
+++ components/WeatherPrediction.js
@@ -2,30 +2,30 @@
 import React, { useContext, useState } from 'react'
 import {Link} from "react-router-dom"
 import {Context} from "../pages/Context"
+import TodayHightlights from "../components/TodayHightlights"
 export default function WeatherPrediction({setIsParams}) {
-    const {weather, isLoading, fahrenheit,setFahrenheit} = useContext(Context)
+    const {weather,fahrenheit,setFahrenheit} = useContext(Context)
     console.log(weather);
     return (
-    <div className="prediction">
-        <div className="temp">
-        <button onClick={() => setFahrenheit(false)}>&deg;C</button>
-        <button onClick={() => setFahrenheit(true)}>&deg;F</button>
+    <div className="weather-container">
+         <div className="temperature-btn">
+                <button className={fahrenheit ? "btn dark-blue" : "btn active"} onClick={() => setFahrenheit(false)}>&deg;C</button>
+                <button className={fahrenheit ? "btn active" : "btn dark-blue"} onClick={() => setFahrenheit(true)}>&deg;F</button>
         </div>
-        {isLoading && <p>Loading...</p>}
         <div className="weather-list">
          {weather.consolidated_weather?.slice(1).map(item => (
-        <Link to={`/details/${item.id}`} key={item.id} onClick={() => setIsParams(true)}>
-            <div className="weather-list-item" key={item.id}>
-                <h4>{new Date(item.applicable_date).toDateString()}</h4>
-                <img src={`https://www.metaweather.com//static/img/weather/${item.weather_state_abbr}.svg`}/>                
-                <ul className="weather-temp">
-                <li>{Math.floor(Math.round(fahrenheit ? (item.max_temp * 9 / 5) + 32: item.max_temp))}&deg;{`${fahrenheit ? 'F' : "C"}`}</li>
-                <li>{Math.floor(Math.round(fahrenheit ? (item.min_temp * 9 / 5) + 32: item.min_temp))}&deg;{`${fahrenheit ? 'F' : "C"}`}</li>
-                </ul>
-            </div>
-        </Link>
+            <Link key={item.id} className="weather-link" to={`/details/${item.id}`} onClick={() => setIsParams(true)}>
+                <div className="weather-list-item" key={item.id}>
+                    <h4>{new Date(item.applicable_date).toDateString()}</h4>
+                    <img src={`https://www.metaweather.com//static/img/weather/${item.weather_state_abbr}.svg`}/>                
+                    <ul className="weather-temp">
+                    <li className="max-temp">{Math.floor(Math.round(fahrenheit ? (item.max_temp * 9 / 5) + 32: item.max_temp))}&deg;{`${fahrenheit ? 'F' : "C"}`}</li>
+                    <li className="min-temp">{Math.floor(Math.round(fahrenheit ? (item.min_temp * 9 / 5) + 32: item.min_temp))}&deg;{`${fahrenheit ? 'F' : "C"}`}</li>
+                    </ul>
+                </div>
+            </Link>
         ))}
-    </div>
+        </div>
     </div>
     )
 }
