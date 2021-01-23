9486b53db7a59d097569d7056e6fbd98e6cdb17a
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-23 09:53:06 +0300

    added footer file

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <p>
+                <span>MetaWeather API</span>
+                <span>|DevChallenges|</span>
+                <span>@manjaka</span>
+            </p>
+        </div>
+    )
+}
